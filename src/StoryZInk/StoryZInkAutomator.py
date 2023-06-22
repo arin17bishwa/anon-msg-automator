56f48f1d127aed351bbe21cab3d23483b10ed6cd
bishwajit <arin17bishwa@gmail.com>
2023-06-23 02:29:52 +0530

    initial commit for support of StoryZInk

---
+++ src/StoryZInk/StoryZInkAutomator.py
@@ -0,0 +1,17 @@
+from abc import ABC
+from typing import Dict
+
+from src.base.BaseMessageAutomator import BaseMessageAutomator
+
+
+class StoryZInkAutomator(BaseMessageAutomator, ABC):
+    @property
+    def _posting_url(self) -> str:
+        return "https://storyzink.com/message_h.php"
+
+    def prepare_payload(self, message: str) -> Dict:
+        return {
+            'name': str(self.uid),
+            'ans1': message,
+            'agree': 'on'
+        }

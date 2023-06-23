b744325b21966f7aa71c9725c196f65431d7c237
bishwajit <arin17bishwa@gmail.com>
2023-06-24 00:49:31 +0530

    added URL and UID patterns to be matched

--- src/StoryZInk/StoryZInkAutomator.py
+++ src/StoryZInk/StoryZInkAutomator.py
@@ -1,10 +1,18 @@
 from abc import ABC
-from typing import Dict
+from typing import Dict, List
 
 from base.BaseMessageAutomator import BaseMessageAutomator
 
 
 class StoryZInkAutomator(BaseMessageAutomator, ABC):
+    @staticmethod
+    def parsable_patterns() -> List[str]:
+        patterns = [
+            r"^https:\/\/storyzink\.com\/m\/(?P<uid>[a-z0-9]{8,8})\/?$",
+            r"^(?P<uid>[a-z0-9]{8,8})",
+        ]
+        return patterns
+
     @property
     def _posting_url(self) -> str:
         return "https://storyzink.com/message_h.php"

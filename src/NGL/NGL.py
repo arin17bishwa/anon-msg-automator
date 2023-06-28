0e0a7f645111115abfbd0482342d7ad33ed0d85f
bishwajit <arin17bishwa@gmail.com>
2023-06-29 02:18:26 +0530

    initial code for NGL spammer

---
+++ src/NGL/NGL.py
@@ -0,0 +1,33 @@
+import uuid
+from abc import ABC
+from typing import List, Dict
+
+from base.BaseMessageAutomator import BaseMessageAutomator
+
+
+class NGL(BaseMessageAutomator, ABC):
+    @staticmethod
+    def parsable_patterns() -> List[str]:
+        patterns = [
+            r"/^https:\/\/confess\.ngl\.link\/(?P<uid>[a-z0-9_\.]+)\/?$",
+            r"(?P<uid>[a-z0-9_]+)",
+        ]
+        return patterns
+
+    @property
+    def _posting_url(self):
+        return 'https://ngl.link/api/submit'
+
+    @staticmethod
+    def _generate_random_device_id() -> str:
+        return str(uuid.uuid4())
+
+    def prepare_payload(self, message: str) -> Dict:
+        payload = {
+            'username': self.uid,
+            'question': message,
+            'deviceId': self._generate_random_device_id(),
+            'gameSlug': 'confessions',
+        }
+
+        return payload

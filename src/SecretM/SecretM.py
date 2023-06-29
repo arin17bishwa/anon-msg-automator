e66f3dc97f381b82c047ac7cd3b547235982524f
bishwajit <arin17bishwa@gmail.com>
2023-06-30 00:36:21 +0530

    initial code for SecretM

---
+++ src/SecretM/SecretM.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from StoryZInk.StoryZInkAutomator import StoryZInkAutomator
+
+
+class SecretMAutomator(StoryZInkAutomator):
+    @staticmethod
+    def parsable_patterns() -> List[str]:
+        patterns = [
+            r"^https:\/\/storyzink\.com\/m\/(?P<uid>[a-z0-9]{8,8})\?s=2$",  # the last ?s=2 part is the only difference
+            r"^(?P<uid>[a-z0-9]{8,8})",
+        ]
+        return patterns
+
+    def _posting_url(self) -> str:
+        return "https://secretm.me/message_h.php"
